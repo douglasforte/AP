38c186a31d41e59e1fa068827688a26b529e5558
Douglas Forte <douglasforte@gmail.com>
2019-02-07 19:40:39 +0000

    CA2 10378710
    Douglas Forte

---
+++ CalculatorTest/CalculatorTestEx/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CalculatorTestEx
+{
+    internal class Calculator
+    {
+        internal object Add(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Subtract(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Multiple(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Divide(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CalculatorTest/CalculatorTestEx/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorTestEx;
+
+namespace CalculatorTestEx
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Calculator calculator = new Calculator();
+
+            var result = calculator.Add(10, 10);
+            Assert.AreEqual(20, result);
+
+            var result2 = calculator.Subtract(30, 10);
+            Assert.AreEqual(20, result);
+
+            var result3 = calculator.Multiple(5, 2);
+            Assert.AreEqual(10, result);
+
+            var result4 = calculator.Divide(10, 2);
+            Assert.AreEqual(5, result);
+
+
+
+        }
+    }
+}
